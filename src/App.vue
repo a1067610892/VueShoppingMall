95eac9c99fdb58928c901d093c8145f50ad0f0a9
unknown <1067610892@qq.com>
2019-07-11 00:02:26 +0800

    导航页面布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <header-nav></header-nav>
+    <list-nav></list-nav>
     <router-view/>
   </div>
 </template>
 
 <script>
 import HeaderNav from '../src/common/HeaderNav'
+import ListNav from '../src/common/ListNav'
 export default {
   name: 'App',
   components: {
-    HeaderNav
+    HeaderNav,
+    ListNav
   }
 }
 </script>
--- src/common/ListNav.vue
+++ src/common/ListNav.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="list-nav">
+    <ul class="list">
+      <li>首页</li>
+      <li>手机</li>
+      <li>官方配件</li>
+      <li>周边产品</li>
+      <li>第三方配件</li>
+      <li>全部商品</li>
+      <li>服务</li>
+    </ul>
   </div>
 </template>
 
@@ -14,5 +23,25 @@ export default {
 </script>
 
 <style scoped>
-
+.list-nav {
+  width: 1000px;
+  height: 60px;
+  margin: 0 auto;
+  border-bottom: 1px solid #989898;
+}
+.list-nav .list {
+  width: 100%;
+  height: 100%;
+}
+.list-nav .list>li {
+  float: left;
+  width: 142px;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  cursor: pointer;
+}
+.list-nav .list>li:hover {
+  color: #5079e1;
+}
 </style>
