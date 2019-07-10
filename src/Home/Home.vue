2b960cd78b41f9706b03e5f8f72fc0fc2b46ea0b
unknown <1067610892@qq.com>
2019-07-10 18:52:40 +0800

    初始化

---
+++ src/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home">
+    111111111111111
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
