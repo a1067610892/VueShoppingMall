09dac1e03a3bc4c454273b938e4978d37a910327
unknown <1067610892@qq.com>
2019-07-10 20:17:27 +0800

    完成头部页面的布局

---
+++ src/common/ListNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="list-nav">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListNav',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
